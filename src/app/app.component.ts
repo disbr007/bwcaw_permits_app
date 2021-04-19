8be86acb4e4eaf14538c7981b0b0947b42c9c1a7
disbr007 <disbr007@umn.edu>
2021-04-19 09:58:17 -0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'bwcaw-permits-app';
+export class AppComponent implements OnInit {
+
+  map: Map;
+
+  ngOnInit(): void {
+    this.map = new Map({
+      view: new View({
+        center: [-10158150.05,6093622.87],
+        zoom: 9,
+      }),
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      target: 'ol-map'
+    });
+  }
 }
